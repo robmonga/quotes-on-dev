7b81657fea71146d807c92ad829f7260d8170fdf
Rob <robmonga2@hotmail.com>
2019-03-03 14:06:12 -0800

    div out page-archive.php

--- themes/quotesondev/page-archives.php
+++ themes/quotesondev/page-archives.php
@@ -21,30 +21,43 @@ get_header(); ?>
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>			
 
+			<section class="list-all-authors">
+			<h3>Quote Authors</h3>
 			<?php
-
        $the_query = new WP_Query(array(
            'post_type'      => 'post',
            'orderby'        => 'title',
            'order'          => 'ASC',
-           'posts_per_page' => -1,
+		   'posts_per_page' => -1,
+		   'format'			=> 'list' //TODO: WHY ISN'T THE LIST THING WORKING?
        )); ?>
-       <div class="list-all-authors">
            <?php  /* Start the Loop */ ?>
            <?php while ($the_query->have_posts()): $the_query->the_post();  ?>
            <?php the_title(sprintf('<span class="each-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></span>'); ?>
-           <?php endwhile;
-       wp_reset_postdata(); ?>
-        </div>
-			
-				<ul>
+           <?php endwhile; wp_reset_postdata(); ?>
+			</section>
+
+				<section class="quote-categories">
+					<h3>Categories</h3>
+					<ul>
 					<?php wp_list_categories( array(
-						'orderby' => 'name'
-						) ); ?> 
-				</ul>
+						'orderby' => 'name',
+						'style'  => 'list',
+						'title_li' => ''
+						) ); ?>
+						</ul> 
+<!-- TODO: add classes thru wp and move some stuff to functions --> 
+					</section>
 
-				<?php wp_tag_cloud( array( 'tags' ) ); ?>
-
+					<section class="tag-cloud">	
+			<h3>Tags</h3>
+				<?php wp_tag_cloud( array(
+					'tags',
+					'smallest'          => 1.5,
+					'largest'           => 1.5,
+					'unit'				=> 'rem',
+					'format'			=> 'list' ) ); ?>
+			</section>
 
 			<?php endwhile; ?>
 
